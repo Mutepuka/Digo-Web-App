3f696ec44864a8f8c992d46f7b47e85c222739b1
Musonda Makena <Musondamakena@gmail.com>
2024-06-13 21:55:46 +0200

    popping font

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,6 @@ import '@styles/globals.css';
 
 const poppins = Poppins({
     subsets:['latin'],
-    display: 'swap',
     variable: '--font-poppins',
     weight: ['100','200','300','400','500','600','700','800','900']
 });
@@ -25,7 +24,7 @@ export const metadata={
 const RootLayout= ({children})=>{
     return(
         <html lang='en'>
-            <body className={poppins.className}>
+            <body className={poppins.variable}>
                 <div className='app'>
                     <SearchFrom/>
                     <Nav/>
