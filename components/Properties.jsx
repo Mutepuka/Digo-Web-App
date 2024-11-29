8c6fabb9fb6ac4ae859ec48aec4ff0675f5aac6e
Musonda Makena <Musondamakena@gmail.com>
2024-11-29 13:55:43 +0200

    changes in latest properties

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -48,62 +48,6 @@ const Properties = () => {
                 <div className="col-md-12">
                     <div className="title-wrap d-flex justify-content-between">
                         <div className="title-box">
-                            <h2 className="title-a">Latest Propertie</h2>
-                        </div>
-                        <div className="title-link">
-                            <Link href="/properties">
-                                All Properties
-                                <span className='bi bi-chevron-right'></span>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Swiper
-            modules={[Pagination]}
-            // spaceBetween={50}
-            // slidesPerView={1}
-            pagination={{ el:'.property-carousel-pagination', clickable: true, type: "bullets" }}
-            autoplay={{ delay: 4000 }}
-            loop={true}
-            breakpoints={{
-                320: {
-                  // For screens >= 320px
-                  slidesPerView: 1,
-                  spaceBetween: 10,
-                },
-                480: {
-                  // For screens >= 480px
-                  slidesPerView: 2,
-                  spaceBetween: 20,
-                },
-                768: {
-                  // For screens >= 768px
-                  slidesPerView: 3,
-                  spaceBetween: 30,
-                },
-                1024: {
-                  // For screens >= 1024px
-                  slidesPerView: 3,
-                  spaceBetween: 40,
-                },
-              }}
-              className='swiper'
-            >
-                {properties && properties.length > 0 && properties.map(data=>(
-                    <SwiperSlide key={data._id}>
-                        <PropertiesCard data={data}/>
-                    </SwiperSlide>
-                ))}
-                <div className="property-carousel-pagination carousel-pagination"></div>
-
-            </Swiper>
-        </div>
-        {/* <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="title-wrap d-flex justify-content-between">
-                        <div className="title-box">
                             <h2 className="title-a">Latest Properties</h2>
                         </div>
                         <div className="title-link">
@@ -116,24 +60,35 @@ const Properties = () => {
                 </div>
             </div>
             <Swiper
-            
-            pagination={{
-                dynamicBullets: true,
-                type: 'bullets',
-                clickable: true,
-                el: 'property-carousel-pagination'
-              }}
+            modules={[Pagination]}
+            pagination={{ el:'.property-carousel-pagination', clickable: true, type: "bullets" }}
+            autoplay={{ delay: 4000 }}
+            loop={true}
               breakpoints={{
                 320:{
                     slidesPerView: 1,
                     spaceBetween: 20
                 },
+                480: {
+                    // For screens >= 480px
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                  },
+                  768: {
+                    // For screens >= 768px
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                  },
+                  1024: {
+                    // For screens >= 1024px
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                  },
                 1200:{
                     slidesPerView: 3,
                     spaceBetween: 20
                 }
               }}
-              modules={[Pagination]}
             className='swiper'
             >
                 {properties && properties.length > 0 && properties.map(data=>(
@@ -141,14 +96,11 @@ const Properties = () => {
                         <PropertiesCard data={data}/>
                     </SwiperSlide>
                 ))}
-                
-
+                <div className="property-carousel-pagination carousel-pagination"></div>
             </Swiper>
-        </div> */}
+        </div>
     </section>
    
-    
-    
   )
 }
 
