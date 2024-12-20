5316938267e0468954433581a555f29ad95ce3e4
Musonda Makena <Musondamakena@gmail.com>
2024-12-20 17:56:56 +0200

    about us paragraph edits commiting

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -71,39 +71,6 @@ const About = () => {
                  </div>
               ))}
             </div>
-           
-
-            {/* <div className="col-md-12 section-t8 position-relative">
-              <div className="row about-affiliate-box">
-                <div className="col-md-6 col-lg-5 ">
-                  <Image
-                  src={aboutAffiliate}
-                  alt='affilate program'
-                  className='img-fluid'
-                  />
-                </div>
-                <div className="col-lg-2 d-none d-lg-block position-relative">
-                  <div className="title-vertical d-flex justify-content-start">
-                    <span>DigoEstate Affiliate Program</span>
-                  </div>
-                </div>
-                <div className="col-md-6 col-lg-5 section-md-t3">
-                  <div className="title-box-d">
-                    <h3 className="title-d">
-                      Join
-                      <span className="color-d"> Program</span>
-                      <br/>become part of the team
-                    </h3>
-                  </div>
-                  <p className="color-text-a">
-                  The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: 
-                  </p>
-                  <p className="color-text-a">
-                  one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words.
-                  </p>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </section>
--- components/AboutUsCard.jsx
+++ components/AboutUsCard.jsx
@@ -1,44 +1,62 @@
 "use client";
 
-import React from 'react';
+import React,{useState} from 'react';
 import { urlFor } from '@libs/sanity';
 
 const AboutUsCard = ({data}) => {
 
-    console.log(data);
+    const [showMore, setShowmore] = useState(false);
+
+    //function to show more
+    const handleShowmore= ()=>{
+        setShowmore((prev) => !prev);
+    }
+
+  // Combine paragraphs into an array
+  const fullContentArray = [data.fparagraph, data.sparagraph, data.tparagraph];
+  const truncatedContent = `${data.fparagraph.substring(0, 800)}...`; 
   return (
     <>
         
-            <div className="col-md-6 col-lg-5 ">
-                <img
-                src={urlFor(data.image)}
-                alt='affilate program'
-                className='img-fluid'
-                />
-            </div>
-            <div className="col-lg-2 d-none d-lg-block position-relative">
-                <div className="title-vertical d-flex justify-content-start">
-                <span>{data.name}</span>
-                </div>
-            </div>
-            <div className="col-md-6 col-lg-5 section-md-t3">
-                <div className="title-box-d">
-                <h3 className="title-d">
-                    {data.fheading}
-                    <span className="color-d"> {data.sheading}</span>
-                    <br/>{data.theading}
-                </h3>
-                </div>
-                <p className="color-text-a">
-                {data.fparagraph} 
-                </p>
-                <p className="color-text-a">
-                {data.sparagraph}
-                </p>
-                <p className="color-text-a">
-                {data.tparagraph}
-                </p>
-            </div>
+    <div className="col-md-6 col-lg-5 ">
+        <img
+        src={urlFor(data.image)}
+        alt='affilate program'
+        className='img-fluid'
+        // width={460}
+        // height={660}
+        />
+    </div>
+    <div className="col-lg-2 d-none d-lg-block position-relative">
+        <div className="title-vertical d-flex justify-content-start">
+        <span>{data.name}</span>
+        </div>
+    </div>
+    <div className="col-md-6 col-lg-5 section-md-t3">
+        <div className="title-box-d">
+        <h3 className="title-d">
+            {data.fheading}
+            <span className="color-d"> {data.sheading}</span>
+            <br/>{data.theading}
+        </h3>
+        </div>
+        <p className="color-text-a">
+        {showMore ? (
+            fullContentArray.map((para, index) => (
+              <React.Fragment key={index}>
+                {para}
+                <br />
+                <br />
+              </React.Fragment>
+            ))
+          ) : (
+            truncatedContent
+          )}
+        </p>
+        <div className="col-md-12 text-center">
+            <button onClick={handleShowmore} className="btn btn-a">  {showMore ? 'Show Less' : 'Show More'}</button>
+        </div>
+    </div>
             </>
   )
 }
