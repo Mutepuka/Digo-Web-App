8f828d853770ff8ec9bc45982c1c322f95ffbc8d
Musonda Makena <Musondamakena@gmail.com>
2024-12-25 22:38:03 +0200

    lifestyle template design commiting

--- app/(services)/lifestyles/page.jsx
+++ app/(services)/lifestyles/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import BreadCrumb from '@components/BreadCrumb';
 import compLogo from '@public/assets/complogo1.jpg';
+import compLogo2 from '@public/assets/complogo2.jpg';
 import Image from '@node_modules/next/image';
+import './style.css';
 
 const HomeDecoration = () => {
   return (
@@ -22,37 +24,60 @@ const HomeDecoration = () => {
               <div className="col-md-6 col-lg-5 ">
                 <Image
                 src={compLogo}
-                width={400}
-                height={400}
+                width={300}
+                height={300}
                 className='img-fluid'
                 alt='company logo'
 
                 />
                 
               </div>
-
-              {/* <div className="col-lg-2 d-none d-lg-block position-relative">
-                <div className="title-vertical d-flex justify-content-start">
-                <span>Company Name</span>
-                </div>
-            </div> */}
-
             <div className="col-md-6 col-lg-5 section-md-t3">
                     <div className="title-box-d">
                     <h3 className="title-d">
-                        Heading
-                        <span className="color-d"> Second Heading</span>
-                        <br/>Third Heading
+                        Furniture 
+                        <span className="color-d"> Fixers</span>
+                        <br/>We make your home beautiful
                     </h3>
                     </div>
                     <p className="color-text-a">
                     Something to write about the company that does the renovation or decoration
                     </p>
-                    
+                    <button type="button" className="btn btn-a" >View Company</button>
                 </div>
 
-
             </div>
+
+            <div className="row">
+
+              {/** company image */}
+              <div className="col-md-6 col-lg-5 ">
+                <Image
+                src={compLogo2}
+                width={300}
+                height={300}
+                className='img-fluid'
+                alt='company logo'
+
+                />
+                
+              </div>
+              <div className="col-md-6 col-lg-5 section-md-t3">
+                    <div className="title-box-d">
+                    <h3 className="title-d">
+                        Unique 
+                        <span className="color-d"> Furniture</span>
+                        <br/>A bit of a magic touch
+                    </h3>
+                    </div>
+                    <p className="color-text-a">
+                    Something to write about the company that does the renovation or decoration
+                    </p>
+                    <button type="button" className="btn btn-a" >View Company</button>
+                </div>
+
+              </div>
+
             </div>
             </div>
           </div>
