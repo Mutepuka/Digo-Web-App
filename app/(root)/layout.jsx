e46ff7cce965860c294cf8d26beeb675f464f662
Musonda Makena <Musondamakena@gmail.com>
2025-03-01 18:44:42 +0200

    clerk bootstrap layout root css commiting

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -4,7 +4,6 @@ import SearchFrom from '@components/SearchFrom';
 import Footer from '@components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import {ClerkProvider,} from '@clerk/nextjs'
 import '@styles/globals.css';
 
 
@@ -15,6 +14,7 @@ const RootLayout= ({children})=>{
         <Nav/>
         {children}
         <Footer/>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </main>
     )
                 
