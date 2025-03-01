aaceca3b954146debad27a4d47f3cb615bdf52b7
Musonda Makena <Musondamakena@gmail.com>
2025-03-01 20:59:27 +0200

    bootstrap js layout root removed commmiting

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -10,11 +10,10 @@ import '@styles/globals.css';
 const RootLayout= ({children})=>{
     return(
         <main id='main'>
-        <SearchFrom/>
-        <Nav/>
-        {children}
-        <Footer/>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            <SearchFrom/>
+            <Nav/>
+            {children}
+            <Footer/>
         </main>
     )
                 
